c6247134f55176285fec9717ff692db6c58b14c7
aliakbaresmaeiliiii <aliakbaresmaeilisoft@gmail.com>
2025-07-15 21:09:50 +0330

    init new components

---
+++ src/app/components/custom-button/custom-button.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-button',
+  imports: [],
+  templateUrl: './custom-button.html',
+  styleUrl: './custom-button.scss'
+})
+export class CustomButton {
+
+}
